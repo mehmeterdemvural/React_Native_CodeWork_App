9ea0b32aeb551e0895874bb5f4fb52f5eb00aaa3
Mehmet Erdem VURAL <mehmeterdemvural@gmail.com>
2023-03-02 03:21:39 +0300

    .env and navigation added

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,45 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-function App() {
+import {SafeAreaView, View, Button, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import Works from '../android/app/src/pages/Works';
+import {API_URL} from '@env';
+
+function HomeScreen({navigation}) {
   return (
-    <SafeAreaView >
-      <Text>Merhaba</Text>
-    </SafeAreaView>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+function App() {
+  const Stack = createNativeStackNavigator();
+  const Drawer = createDrawerNavigator();
+  const Tab = createBottomTabNavigator();
+  console.log(API_URL);
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="WorksPage" component={Works} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Notifications" component={NotificationsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 export default App;
